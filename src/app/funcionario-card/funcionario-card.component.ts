ecd93e48bb648c0e7cac4b4efc9394ad01115276
vidotti <vidotti.ai@gmail.com>
2019-08-09 18:10:22 -0300

    alterando estilos dinamicamente com ngStyles

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  styleUrls: ['./funcionario-card.component.css']
+  //styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+      .card-body {
+        text-transform: uppercase;
+        color: blue;
+        font-weight: bold;
+      }
+    `]
 })
 export class FuncionarioCardComponent {
 
   @Input('obj') funcionario: any;
+
+
+  getEstilosCartao() {
+    return {
+      'border-width.px': this.funcionario.id,
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+    };
+  }
 
 
 
